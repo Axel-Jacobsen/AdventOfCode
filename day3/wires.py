4ed42705282703709d28a6d21c654ee295f7ca51
Axel-Jacobsen <axelnj44@gmail.com>
2019-12-16 19:03:02 +0100

    day3 part2

--- day3/wires.py
+++ day3/wires.py
@@ -7,11 +7,12 @@ Pos = namedtuple('Pos', ('x', 'y'))
 
 class Wire(object):
 
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2, path_length):
         """ low and start are named tuples
         """
         self.start = p1
         self.end = p2
+        self.path_length = path_length
         self.is_vertical = (p1.x == p2.x)
         self.x = p1.x if self.is_vertical else None
         self.y = p2.y if not self.is_vertical else None
@@ -24,29 +25,26 @@ class Wire(object):
         """
         # If wires are parallel, no intersections
         if self.is_vertical == other.is_vertical:
-            assert not ((self.x == other.x and self.x != 0 and self.x is not None) or (self.y == other.y and self.y != 0 and self.y is not None)), 'coincident lines encountered'
+            assert not ((self.x == other.x and self.x != 0 and self.x is not None)
+                    or (self.y == other.y and self.y != 0 and self.y is not None)), 'coincident lines encountered'
             return None
 
-        other_low_x  = min(other.start.x, other.end.x)
-        other_high_x = max(other.start.x, other.end.x)
-        other_low_y  = min(other.start.y, other.end.y)
-        other_high_y = max(other.start.y, other.end.y)
-        self_low_x  = min(self.start.x, self.end.x)
-        self_high_x = max(self.start.x, self.end.x)
-        self_low_y  = min(self.start.y, self.end.y)
-        self_high_y = max(self.start.y, self.end.y)
+        other_low_x, other_high_x, other_low_y, other_high_y = Wire.bounding_points(other)
+        self_low_x, self_high_x, self_low_y, self_high_y = Wire.bounding_points(self)
 
         # Check this line being vertical and other horizontal
         if self.is_vertical:
-            if (other_low_x <= self.x <= other_high_x
-                and self_low_y <= other.y <= self_high_y):
+            if (other_low_x <= self.x <= other_high_x and self_low_y <= other.y <= self_high_y):
                 return Pos(self.x, other.y)
         else:
-            if (other_low_y <= self.y <= other_high_y
-                and self_low_x <= other.x <= self_high_x):
+            if (other_low_y <= self.y <= other_high_y and self_low_x <= other.x <= self_high_x):
                 return Pos(other.x, self.y)
 
         return None
+
+    @staticmethod
+    def bounding_points(w):
+        return (min(w.start.x, w.end.x), max(w.start.x, w.end.x), min(w.start.y, w.end.y), max(w.start.y, w.end.y))
 
 
 def get_paths(fname):
@@ -58,22 +56,51 @@ def get_paths(fname):
 def path_to_wires(directions):
     wire_set = []
     curr_x, curr_y = 0, 0
+    path_length = 0
     for dirx in directions:
         if dirx[0] == 'U':
-            wire_set.append(Wire(Pos(curr_x, curr_y), Pos(curr_x, curr_y + int(dirx[1:]))))
+            wire_set.append(
+                    Wire(
+                        Pos(curr_x, curr_y),
+                        Pos(curr_x, curr_y + int(dirx[1:])),
+                        path_length
+                        )
+                    )
             curr_x, curr_y = curr_x, curr_y + int(dirx[1:])
         elif dirx[0] == 'D':
-            wire_set.append(Wire(Pos(curr_x, curr_y), Pos(curr_x, curr_y - int(dirx[1:]))))
+            wire_set.append(
+                    Wire(
+                        Pos(curr_x, curr_y),
+                        Pos(curr_x, curr_y - int(dirx[1:])),
+                        path_length
+                        )
+                    )
             curr_x, curr_y = curr_x, curr_y - int(dirx[1:])
         elif dirx[0] == 'L':
-            wire_set.append(Wire(Pos(curr_x, curr_y), Pos(curr_x - int(dirx[1:]), curr_y)))
+            wire_set.append(
+                    Wire(
+                        Pos(curr_x, curr_y),
+                        Pos(curr_x - int(dirx[1:]), curr_y),
+                        path_length
+                        )
+                    )
             curr_x, curr_y = curr_x - int(dirx[1:]), curr_y
         elif dirx[0] == 'R':
-            wire_set.append(Wire(Pos(curr_x, curr_y), Pos(curr_x + int(dirx[1:]), curr_y )))
+            wire_set.append(
+                    Wire(
+                        Pos(curr_x, curr_y),
+                        Pos(curr_x + int(dirx[1:]), curr_y),
+                        path_length
+                        )
+                    )
             curr_x, curr_y = curr_x + int(dirx[1:]), curr_y
+        path_length += int(dirx[1:])
     return wire_set
 
-def dist(fname):
+def point_distance(p1, p2):
+    return abs(p2.x - p1.x) + abs(p2.y - p1.y)
+
+def get_manhattan_dist(fname):
     path1, path2 = get_paths(fname)
     wireset1 = path_to_wires(path1)
     wireset2 = path_to_wires(path2)
@@ -81,13 +108,43 @@ def dist(fname):
     for w1 in wireset1:
         for w2 in wireset2:
             cross = w1.intersects(w2)
-            assert cross == w2.intersects(w1)
-            if cross:
-                min_dist = min(min_dist, abs(cross.x) + abs(cross.y)) if (cross.x != 0 and cross.y != 0) else min_dist
+            if not cross:
+                continue
+            d = point_distance(cross, Pos(0,0))
+            if d < min_dist and d != 0:
+                min_dist  = d 
 
     return min_dist
 
+def get_path_dist(fname):
+    path1, path2 = get_paths(fname)
+    wireset1 = path_to_wires(path1)
+    wireset2 = path_to_wires(path2)
+    min_pathlen = 1000000
+    for w1 in wireset1:
+        for w2 in wireset2:
+            cross = w1.intersects(w2)
+            if not cross:
+                continue
+            d = point_distance(cross, Pos(0,0))
+            pathlen = w1.path_length + point_distance(w1.start, cross) + \
+                      w2.path_length + point_distance(w2.start, cross)
+            if pathlen < min_pathlen and d != 0:
+                min_pathlen = pathlen
+    return min_pathlen
 
 if __name__ == '__main__':
-    print('FINAL:', dist('paths.txt'))
+    v1 = get_path_dist('testp1.txt')
+    v2 = get_manhattan_dist('testp1.txt')
+    v3 = get_path_dist('testp2.txt') 
+    v4 = get_manhattan_dist('testp2.txt')
+    print(v1, '<-', 610)
+    print(v2, '<-', 159)
+    print(v3, '<-', 410)
+    print(v4, '<-', 135)
+
+    min_dist = get_manhattan_dist('paths.txt')
+    pathlen = get_path_dist('paths.txt')
+    print('FINAL MANHATTAN DISTANCE:', min_dist)
+    print('SUM OF PATH LENGTHS:     ', pathlen)
 
