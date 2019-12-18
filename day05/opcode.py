d64aa6c361feec4c9ca9bf54e89389787e50cbfe
Axel-Jacobsen <axelnj44@gmail.com>
2019-12-18 01:11:05 +0100

    day 5 part1 done
    day 5 part 1 done

---
+++ day05/opcode.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python3
+
+
+def get_prog(fname):
+    with open(fname, 'r') as f:
+        return [int(s) for s in f.readline().strip().split(',')]
+
+def opcode(r: list):
+    f = list(r)
+    pc = 0
+
+    def get_v(v, parameter_mode):
+        if parameter_mode == 0: # position mode
+            return f[f[v]]
+        if parameter_mode == 1: # immediate mode
+            return f[v]
+        raise RuntimeError(f'Unexpected parameter mode: {parameter_mode}')
+
+    def gord(arr, v):
+        if v < len(arr):
+            return int(arr[v])
+        else:
+            return 0
+
+    while True:
+        opcode = str(f[pc])[-2:]
+
+        if len(opcode) == 1:
+            opcode = '0' + opcode
+
+        pms = str(f[pc])[:-2][::-1]
+
+        if opcode == '01':
+            f[f[pc + 3]] = get_v(pc + 1, gord(pms, 0)) + get_v(pc + 2, gord(pms, 1))
+            pc += 4
+
+        elif opcode == '02':
+            f[f[pc + 3]] = get_v(pc + 1, gord(pms, 0)) * get_v(pc + 2, gord(pms, 1))
+            pc += 4
+
+        elif opcode == '03':
+            f[f[pc + 1]] = int(input('> '))
+            pc += 2
+
+        elif opcode == '04':
+            print(get_v(pc + 1, gord(pms, 0)))
+            pc += 2
+
+        elif opcode == '99':
+            # Program is done
+            break
+
+        else:
+            # Illegal Opcode
+            # Or something else is wrong
+            raise RuntimeError(f'Illegal Opcode {opcode} | PC {pc}')
+
+    return f
+
+
+if __name__ == '__main__':
+    f  = get_prog('prog.txt')
+    mf = opcode(f)
+
